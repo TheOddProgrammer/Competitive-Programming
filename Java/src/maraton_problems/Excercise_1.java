b81eca4b841df368d92a529664f56fb393c09289
TheOddProgrammer <juangarzonmani@gmail.com>
2023-03-13 06:49:33 -0500

    Primeros Ejercicios

---
+++ Java/src/maraton_problems/Excercise_1.java
@@ -0,0 +1,11 @@
+package maraton_problems;
+
+public class Excercise_1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!!");
+        
+    }
+    
+}
