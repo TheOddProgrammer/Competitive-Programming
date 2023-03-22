f69c871788a79f19bcf925d41b487d54f9fe6dde
TheOddProgrammer <juangarzonmani@gmail.com>
2023-03-22 09:41:04 -0500

    Mas Ejercicios

--- Java/src/maraton_problems/Testeos.java
+++ Java/src/maraton_problems/Testeos.java
@@ -1,13 +1,53 @@
 package maraton_problems;
-import java.util.*;
-public class Testeos {
+import java.util.Scanner;
+class HelloWorld {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner text = new Scanner(System.in);
+        int M = input.nextInt();
+        int N = input.nextInt();
+        int[][] tablero = new int[M][N];
+
+        for(int i=0;i<M;i++) {
+            for(int j=0;j<N;j++) {
+                tablero[i][j] = (int)(Math.random() * (1000+1)); 
+            }
+        }
+
+        for(int i=0;i<M;i++) { //Quitar
+            for(int j=0;j<N;j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        int K = input.nextInt();
+        String[] Kv = text.nextLine().split("");
         
-        String nombre = "Juan";
+        for (int l = 1; l <=K; l++) {
+            for(int k = 0; k < Kv.length; k++) {
+                if (Kv[k].equals("V")) {
+                    for (int i = M-1; i>=10; i--) {
+                        for (int j = 0; j < N; j++) {
+                            tablero[i-M][j] = tablero[i][j];
+                        }
+                    }
+                } else {
+                    for (int i = 0; i<M; i++) {
+                        for (int j = N-1; j >= N; j--) {
+                            tablero[M-i][j] = tablero[i][j];
+                        }
+                    }
+                }
+            }
+        }
         
-        String[] letras = nombre.split("");
+        for(int i=0;i<M;i++) { //Quitar
+            for(int j=0;j<N;j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println("");
+        }
         
-        System.out.println(letras[1]);
     }
 }
